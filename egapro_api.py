0de3ae3131c8f429bebd86c28990f81f90dd0282
DorianTardieu <dorian.tardieu@saintmichelannecy.fr>
2024-06-11 15:51:36 +0200

    Correction Service RPC

--- egapro_api.py
+++ egapro_api.py
@@ -1,30 +1,16 @@
 import rpyc
-from csv import DictReader
- 
-egapro_data = {}
- 
-with open("index-egalite-fh-utf8.csv") as csv:
-    reader = DictReader(csv, delimiter=";", quotechar='"')
-    for row in reader:
-        if egapro_data.get(row["SIREN"]) is None:
-            egapro_data[row["SIREN"]] = row
-        elif egapro_data[row["SIREN"]]["Année"] < row["Année"]:
-            egapro_data[row["SIREN"]].update(row)
- 
+
 class EgaProService(rpyc.Service):
-    def on_connect(self, conn):
-        pass
- 
-    def on_disconnect(self, conn):
-        pass
- 
     def exposed_get_data(self, siren):
-        """
-        Retourne les données EgaPro pour un numéro SIREN donné.
-        """
-        return egapro_data.get(str(siren), "SIREN not found")
- 
+        with open("index-egalite-fh-utf8.csv", encoding="utf-8") as csv_file:
+            reader = DictReader(csv_file, delimiter=";", quotechar='"')
+            for row in reader:
+                if row["SIREN"] == siren:
+                    return row
+        return None
+
 if __name__ == "__main__":
     from rpyc.utils.server import ThreadedServer
-    t = ThreadedServer(EgaProService, port=18861)
-    t.start()
+    server = ThreadedServer(EgaProService, port=18861)
+    print("RPC server running on port 18861")
+    server.start()
