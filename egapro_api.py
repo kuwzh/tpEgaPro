ad769156a60355abbeee448b64520e75014ca46e
kuwzh <emploiepaul5@gmail.com>
2024-06-11 15:52:39 +0200

    Correction de l'API REST

--- egapro_api.py
+++ egapro_api.py
@@ -1,47 +1,25 @@
 from csv import DictReader
 from flask import Flask, jsonify
-from flask_swagger_ui import get_swaggerui_blueprint
 
-# Lire le fichier CSV et stocker les données dans un dictionnaire
+app = Flask(__name__)
+
 egapro_data = {}
 
-with open("index-egalite-fh-utf8.csv") as csv:
-    reader = DictReader(csv, delimiter=";", quotechar='"')
+# Lecture des données depuis le fichier CSV lors du chargement de l'application
+with open("index-egalite-fh-utf8.csv", encoding="utf-8") as csv_file:
+    reader = DictReader(csv_file, delimiter=";", quotechar='"')
     for row in reader:
         if egapro_data.get(row["SIREN"]) is None:
             egapro_data[row["SIREN"]] = row
         elif egapro_data[row["SIREN"]]["Année"] < row["Année"]:
             egapro_data[row["SIREN"]].update(row)
 
-application = Flask(__name__)
+@app.route("/api/siren/<int:siren>", methods=["GET"])
+def get_data_by_siren(siren):
+    data = egapro_data.get(str(siren))
+    if not data:
+        return jsonify({"error": "SIREN not found"}), 404
+    return jsonify(data), 200
 
-# Définir la route SIREN
-@application.route("/siren/<siren>")
-def siren(siren: int):
-    """
-    Retourne les données EgaPro pour un numéro SIREN donné.
-    Renvoie une 404 si le SIREN n'est pas trouvé.
-
-    :param siren: Numéro SIREN en tant qu'entier
-    :return: Les données correspondantes sous forme de JSON
-    """
-    response = egapro_data.get(siren)
-
-    if response is None:
-        response = {"error": "SIREN not found"}
-        status = 404
-    else:
-        status = 200
-    return jsonify(response), status
-
-# Configuration de Swagger UI
-SWAGGER_URL = '/api/docs'  # URL pour accéder à la documentation Swagger UI
-API_URL = '/swagger.json'   # URL de votre documentation Swagger JSON
-
-swaggerui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': "EgaPro API"
-    }
-)
+if __name__ == "__main__":
+    app.run(debug=True)
